9c9f435e4acd4683735660ab45c6ba0b3f6f006f
ihor <evroigor86@gmail.com>
2023-02-13 15:13:20 +0200

    change port

--- server/index.js
+++ server/index.js
@@ -8,36 +8,40 @@ router.use(cors());
 const prisma = new PrismaClient();
 
 router.get('/apartments', async (req, res) => {
-  const apartments = await prisma.apartment.findMany({
-    skip: 20 * (+req.query.page - 1),
-    take: 20,
-  });
-  const apartmentsCount = await prisma.apartment.count({});
+  try {
+    const apartments = await prisma.apartment.findMany({
+      skip: 20 * (+req.query.page - 1),
+      take: 20,
+    });
+    const apartmentsCount = await prisma.apartment.count({});
 
-  let images = {};
-  for (const apartment of apartments) {
-    const apartmentImages = await prisma.image.findMany({
-      where: {
-        apartmentId: {
-          equals: apartment.id,
+    let images = {};
+    for (const apartment of apartments) {
+      const apartmentImages = await prisma.image.findMany({
+        where: {
+          apartmentId: {
+            equals: apartment.id,
+          },
         },
-      },
+      });
+
+      images[apartment.id] = apartmentImages;
+    }
+
+    const imagesApartments = apartments.map(apartment => {
+      return {
+        ...apartment,
+        images: images[apartment.id],
+      };
     });
 
-    images[apartment.id] = apartmentImages;
+    res.json({
+      pages: Math.ceil(apartmentsCount / 20),
+      apartments: imagesApartments,
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
   }
-
-  const imagesApartments = apartments.map(apartment => {
-    return {
-      ...apartment,
-      images: images[apartment.id],
-    };
-  });
-
-  res.json({
-    pages: Math.ceil(apartmentsCount / 20),
-    apartments: imagesApartments,
-  });
 });
 
 router.listen(3000);
